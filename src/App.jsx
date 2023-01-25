1d875547421aab515d8d52f3c76e0c97a7d2e0a6
Paolo Guray <froiodpaolo@gmail.com>
2023-01-25 14:28:45 +0800

    route pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+import Create from './pages/create/Create'
+import Home from './pages/home/Home'
+
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>Hello Vite</h1>
+    <div className='min-h-full'>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+        <Routes>
+          <Route exact path="/create" element={<Create/>} />
+        </Routes>
+        <Routes>
+          <Route exact path="/search" element={<Search/>} />
+        </Routes>
+        <Routes>
+          <Route exact path="/recipe/:id" element={<Recipe/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
