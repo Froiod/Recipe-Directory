c38d610a39e37c56ebbc282062593ac2129eacd6
Paolo Guray <froiodpaolo@gmail.com>
2023-01-26 13:28:06 +0800

    added recipes and styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {useFetch} from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 
 const Home = () => {
+  const {data, isPending, error} = useFetch('http://localhost:3000/recipes')
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='max-w-[1200px] my-16 mx-auto'>
+      {error && <p className='load-err'>{error}</p>}
+      {isPending && <p className='load-err'>Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
     </div>
   )
 }
