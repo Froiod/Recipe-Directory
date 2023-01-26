add71bd2eea5035718c4266d486fb27fbc5b39d3
Paolo Guray <froiodpaolo@gmail.com>
2023-01-26 15:37:07 +0800

    added create form

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Create = () => {
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
   return (
-    <div>Create</div>
+    <div className='text-gray-600 margin max-w-lg'>
+      <h2 className='text-center text-gray-700 margin text-2xl font-bold'>Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <span className='span'>Recipe Title:</span>
+          <input 
+            type="text"  
+            onChange={(e)=> setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span className='span'>Recipe Method:</span>
+          <textarea 
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span className='span'>Cooking Time (minutes):</span>
+          <input 
+            type="number"
+            onChange={(e)=> setCookingTime(e.target.value)}
+            value={cookingTime}
+            required 
+          />
+        </label>
+
+        <button className='button'>Submit</button>
+
+      </form>
+    </div>
   )
 }
 
