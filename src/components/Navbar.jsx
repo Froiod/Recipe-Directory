f611a9d94e22e556b7dabe44fff21b1332cf6bd7
Paolo Guray <froiodpaolo@gmail.com>
2023-01-27 16:10:34 +0800

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='bg-[#58249c] p-5 text-white'>
+    <div className='bg-[#58249c] p-4 text-white'>
       <nav className='flex items-center mx-auto max-w-[1200px]'>
         <Link to="/" className='mr-auto'>
           <h1 className='text-3xl font-bold'>Cooking Ina</h1>
         </Link>
-        <Link to="/create" className='ml-10 p-2 border border-white rounded hover:bg-white  hover:text-gray-700'>
+        <Link to="/create" className='p-2 border border-white rounded hover:bg-white  hover:text-gray-700'>
           <h1>Create Recipe</h1>
         </Link>
       </nav>
