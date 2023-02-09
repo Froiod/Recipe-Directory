9d24a9d94825d76acb328d27446e7c8070498cd5
Paolo Guray <froiodpaolo@gmail.com>
2023-02-09 23:49:18 +0800

    installed react-icons

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { FaTimes } from 'react-icons/fa';
 import {Link} from 'react-router-dom'
 
 const RecipeList = ({recipes}) => {
@@ -14,7 +15,7 @@ const RecipeList = ({recipes}) => {
     <div className='grid gap-10 max-w-[1200px] md:grid-cols-3 grid-cols-2 my-10 mx-auto'>
       {list.map((recipe) => (
         <div key={recipe.id} className='relative bg-white text-gray-700 p-5 rounded shadow-md shadow-black transition-transform ease-linear duration-300 hover:rotate-2'>
-          <button className='absolute top-3 right-3 cursor-pointer opacity-50 hover:opacity-100' onClick={() => handleClick(recipe.id)}>x</button>
+          <button className='absolute top-3 right-3 cursor-pointer opacity-50 hover:opacity-100' onClick={() => handleClick(recipe.id)}><FaTimes/></button>
           <h3 className='text-gray-700 text-2xl font-bold mb-1'>{recipe.title}</h3>
           <p className='text-gray-600 text-lg'>{recipe.cookingTime} to make</p>
           <div className='text-gray-700 text-base my-5 leading-6'>{recipe.method.substring(0, 100)}...</div>
